e085860e288c456cfef8d68e5290e400fb35f14f
charleswg <charleswghk@gmail.com>
2021-04-13 16:48:42 -0500

    Update keras_to_tensorflow.py

--- keras_to_tensorflow.py
+++ keras_to_tensorflow.py
@@ -24,6 +24,43 @@ from keras_gcnn.layers.pooling import GroupPool
 
 from groupy.gconv.tensorflow_gconv.splitgconv2d import gconv2d_util, gconv2d
 
+import keras.backend as K
+
+def w_dice_coeff(y_true, y_pred):
+    weight=1
+    smooth = 1
+    w, m1, m2 = weight * weight, y_true, y_pred
+    intersection = (m1 * m2)
+    score = (2. * K.sum(w * intersection) + smooth) / (K.sum(w * m1) + K.sum(w * m2) + smooth)
+    return score
+
+
+def weighted_dice_loss(y_true, y_pred):
+    y_true = K.cast(y_true, 'float32')
+    y_pred = K.cast(y_pred, 'float32')
+    # if we want to get same size of output, kernel size must be odd number
+    if K.int_shape(y_pred)[1] == 128:
+        kernel_size = 11
+    elif K.int_shape(y_pred)[1] == 256:
+        kernel_size = 21
+    elif K.int_shape(y_pred)[1] == 512:
+        kernel_size = 21
+    elif K.int_shape(y_pred)[1] == 1024:
+        kernel_size = 41
+    else:
+        raise ValueError('Unexpected image size')
+    averaged_mask = K.pool2d(
+        y_true, pool_size=(kernel_size, kernel_size), strides=(1, 1), padding='same', pool_mode='avg')
+    border = K.cast(K.greater(averaged_mask, 0.005), 'float32') * K.cast(K.less(averaged_mask, 0.995), 'float32')
+    weight = K.ones_like(averaged_mask)
+    w0 = K.sum(weight)
+    weight += border * 2
+    w1 = K.sum(weight)
+    weight *= (w0 / w1)
+    loss = 1 - weighted_dice_coeff(y_true, y_pred, weight)
+    return loss
+  
+  
 K.set_learning_phase(0)
 FLAGS = flags.FLAGS
 
